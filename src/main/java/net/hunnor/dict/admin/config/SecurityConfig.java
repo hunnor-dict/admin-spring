f08773ee805d8abfe6223f1a201d01538ef903e7
Ádám Z. Kövér <adamzkover@gmail.com>
2026-02-21 14:33:49 +0100

    Fix checkstyle error

--- src/main/java/net/hunnor/dict/admin/config/SecurityConfig.java
+++ src/main/java/net/hunnor/dict/admin/config/SecurityConfig.java
@@ -2,9 +2,9 @@ package net.hunnor.dict.admin.config;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
